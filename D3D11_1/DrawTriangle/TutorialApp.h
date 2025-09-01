703eb3357be1f608c996957231bda8dfc5ddf332
mob954325 <lsh954325@gmail.com>
2025-09-01 14:16:19 +0900

    add drawTriangle project

---
+++ D3D11_1/DrawTriangle/TutorialApp.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include <d3d11.h>
+
+#include "../Common/GameApp.h"
+
+class TutorialApp : public GameApp
+{
+public:
+	TutorialApp(HINSTANCE hInstance);
+	~TutorialApp();
+
+	ID3D11Device* m_pDevice = nullptr;					// 디바이스
+	ID3D11DeviceContext* m_pDeviceContext = nullptr;	// 디바이스 컨텍스트
+	// IDXGISwapChain1* m_pSwapChain = nullptr;					// 스왑체인 // ???
+};
