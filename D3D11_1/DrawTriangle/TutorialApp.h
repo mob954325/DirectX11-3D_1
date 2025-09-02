ea627095d5d9a0ca575e7d354d3aca1bf4206a64
mob954325 <lsh954325@gmail.com>
2025-09-02 11:45:14 +0900

    1. 화면 지우기 업로드

--- D3D11_1/DrawTriangle/TutorialApp.h
+++ D3D11_1/DrawTriangle/TutorialApp.h
@@ -1,5 +1,6 @@
 ﻿#pragma once
 #include <d3d11.h>
+#include <dxgi1_2.h>
 
 #include "../Common/GameApp.h"
 
@@ -9,7 +10,15 @@ public:
 	TutorialApp(HINSTANCE hInstance);
 	~TutorialApp();
 
-	ID3D11Device* m_pDevice = nullptr;					// 디바이스
-	ID3D11DeviceContext* m_pDeviceContext = nullptr;	// 디바이스 컨텍스트
-	// IDXGISwapChain1* m_pSwapChain = nullptr;					// 스왑체인 // ???
+	ID3D11Device* m_pDevice = nullptr;						// 디바이스
+	ID3D11DeviceContext* m_pDeviceContext = nullptr;		// 디바이스 컨텍스트
+	IDXGISwapChain1* m_pSwapChain = nullptr;				// 스왑체인 
+	ID3D11RenderTargetView* m_pRenderTargetView = nullptr;	// 랜더 타겟
+
+	virtual bool Initialize(UINT Width, UINT Height);	// 윈도우 정보는 게임 마다 다를수 있으므로 등록,생성,보이기만 한다.
+	virtual void Update();
+	virtual void Render();
+
+	bool InitD3D();
+	void UninitD3D();
 };
