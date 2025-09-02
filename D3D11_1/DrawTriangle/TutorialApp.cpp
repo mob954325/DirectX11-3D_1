87ba27f431ec48b10c6d1ffd9df8a4dd20bc2faa
mob954325 <lsh954325@gmail.com>
2025-09-02 14:35:48 +0900

    2. 삼각형 그리기 추가

--- D3D11_1/DrawTriangle/TutorialApp.cpp
+++ D3D11_1/DrawTriangle/TutorialApp.cpp
@@ -1,4 +1,4 @@
-﻿#include "TutorialApp.h"
+#include "TutorialApp.h"
 #include "../Common/Helper.h"
 
 #include <directxtk/SimpleMath.h>
@@ -9,10 +9,18 @@
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "dxgi.lib")
 
+#include <d3dcompiler.h>
+#pragma comment(lib, "d3dcompiler.lib")
+
+
 using namespace DirectX::SimpleMath;
 using Microsoft::WRL::ComPtr;
 
-#define USE_FLIPMODE 1 // 경고 메세지를 없애려면 Flip 모드를 사용한다.
+// vector3 정점 구조체
+struct Vertex
+{
+	Vector3 position;
+};
 
 TutorialApp::TutorialApp(HINSTANCE hInstance)
 	: GameApp(hInstance)
@@ -22,6 +30,7 @@ TutorialApp::TutorialApp(HINSTANCE hInstance)
 
 TutorialApp::~TutorialApp()
 {
+	UninitScene();
 	UninitD3D();
 }
 
@@ -30,6 +39,9 @@ bool TutorialApp::Initialize(UINT Width, UINT Height)
 	__super::Initialize(Width, Height);
 
 	if (!InitD3D())
+		return false;
+
+	if (!InitScene())
 		return false;
 
 	return true;
@@ -41,15 +53,20 @@ void TutorialApp::Update()
 
 void TutorialApp::Render()
 {
-#if USE_FLIPMODE == 1
-	// Flip 모드에서는 매프레임 설정해야한다.
-	m_pDeviceContext->OMSetRenderTargets(1, &m_pRenderTargetView, NULL);
-#endif	
-
 	Color color(0.1f, 0.2f, 0.3f, 1.0f);
 
 	// 화면 칠하기.
 	m_pDeviceContext->ClearRenderTargetView(m_pRenderTargetView, color);
+
+	// Draw 계열 함수를 호출 하기 전에 렌더링 파이프라인에 필수 스테이지 설정을 해야한다.
+	m_pDeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);	// 정점을 이어서 그릴 방식 설정
+	m_pDeviceContext->IASetVertexBuffers(0, 1, &m_pVertexBuffer, &m_VertexBufferStride, &m_VertexBufferOffset);
+	m_pDeviceContext->IASetInputLayout(m_pInputLayout);
+	m_pDeviceContext->VSSetShader(m_pVertexShader, nullptr, 0);
+	m_pDeviceContext->PSSetShader(m_pPixelShader, nullptr, 0);
+
+	// render a triangle
+	m_pDeviceContext->Draw(m_VertexCount, 0);
 
 	// 스왑체인 교체
 	m_pSwapChain->Present(0, 0);
@@ -57,95 +74,51 @@ void TutorialApp::Render()
 
 bool TutorialApp::InitD3D()
 {
-	HRESULT hr = S_OK;
+	// 결과값.
+	HRESULT hr = 0;
 
-	// 1. D3D11 Device, DeviceContext 생성
-	
-	// https://learn.microsoft.com/ko-kr/windows/win32/api/d3d11/ne-d3d11-d3d11_create_device_flag
-	UINT creationFlag = D3D11_CREATE_DEVICE_BGRA_SUPPORT;
+	// 스왑체인 속성 설정 구조체 생성.
+	DXGI_SWAP_CHAIN_DESC swapDesc = {};
+	swapDesc.BufferCount = 1;
+	swapDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapDesc.OutputWindow = m_hWnd;	// 스왑체인 출력할 창 핸들 값.
+	swapDesc.Windowed = true;		// 창 모드 여부 설정.
+	swapDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	// 백버퍼(텍스처)의 가로/세로 크기 설정.
+	swapDesc.BufferDesc.Width = m_ClientWidth;
+	swapDesc.BufferDesc.Height = m_ClientHeight;
+	// 화면 주사율 설정.
+	swapDesc.BufferDesc.RefreshRate.Numerator = 60;
+	swapDesc.BufferDesc.RefreshRate.Denominator = 1;
+	// 샘플링 관련 설정.
+	swapDesc.SampleDesc.Count = 1;
+	swapDesc.SampleDesc.Quality = 0;
 
+	UINT creationFlags = 0;
 #ifdef _DEBUG
-	/// https://learn.microsoft.com/ko-kr/windows/win32/direct3d11/overviews-direct3d-11-devices-layers
-	/// 디버그 계층을 지원하는 디바이스를 만들도록 플래그 추가
-	/// 
-	/// 디버그 계층은 광범위한 추가 매개 변수 및 일관성 유효성 검사(예: 셰이더 링크 및 리소스 바인딩 유효성 검사, 매개 변수 일관성 유효성 검사 및 오류 설명 보고)를 제공합니다.
-	/// 해당 설정을 사용할 경우 애플리케이션이 상당히 느려진다.
-	creationFlag |= D3D11_CREATE_DEVICE_DEBUG; 
-#endif //  _DEBUG
-
-	// 그래픽 카드 하드웨어의 스펙으로 호환되는 가장 높은 DirectX 기능레벨로 생성하여 드라이버가 작동한다.
-	// 인터페이스는 Direct3D11이지만 GPU 드라이버는 D3D12 드라이버가 작동될 수 있다.
-	D3D_FEATURE_LEVEL featureLevels[] =
-	{	// 0번 index부터 순서대로 시도
-		D3D_FEATURE_LEVEL_12_2,D3D_FEATURE_LEVEL_12_1,D3D_FEATURE_LEVEL_12_0,D3D_FEATURE_LEVEL_11_1,D3D_FEATURE_LEVEL_11_0
-	};
-	D3D_FEATURE_LEVEL actualFeatureLevel;	// 최종 feature level 저장 변수
-
-	HR_T(D3D11CreateDevice
-	(
-		nullptr,
-		D3D_DRIVER_TYPE_HARDWARE,
-		0,
-		creationFlag,
-		featureLevels,
-		ARRAYSIZE(featureLevels),
-		D3D11_SDK_VERSION,
-		&m_pDevice,
-		&actualFeatureLevel,
-		&m_pDeviceContext
-	));
-
-
-	// 2. 스왑체인 생성을 위한 DXGI Factory 생성
-	UINT dxgiFactoryFlags = 0;
-
-#ifdef _DEBUG
-	dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
-#endif // _DEBUG
-
-	ComPtr<IDXGIFactory2> pFactory;
-	HR_T(CreateDXGIFactory2(dxgiFactoryFlags, IID_PPV_ARGS(&pFactory)));
-
-	DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
-#if USE_FLIPMODE == 1
-	swapChainDesc.BufferCount = 2;
-	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
-#else
-	swapChainDesc.BufferCount = 1;
-	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	creationFlags |= D3D11_CREATE_DEVICE_DEBUG;
 #endif
-	swapChainDesc.Width = m_ClientWidth;
-	swapChainDesc.Height = m_ClientHeight;
+	// 1. 장치 생성.   2.스왑체인 생성. 3.장치 컨텍스트 생성.
+	HR_T(D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, creationFlags, NULL, NULL,
+		D3D11_SDK_VERSION, &swapDesc, &m_pSwapChain, &m_pDevice, NULL, &m_pDeviceContext));
 
-	// 하나의 픽셀이 채널 RGBA 각 8비트 형식으로 표현
-	// Unsigned Normalized Integer 8비트 정수(0~255)단계를 부동소수점으로 정규화한 0.0~1.0으로 매핑하여 표현한다.
-	swapChainDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
-	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT; // 스왑 체인의 백 버퍼가 렌더링 파이프라인의 최종 출력 대상으로 사용
-	swapChainDesc.SampleDesc.Count = 1;	// 멀티 샘플링 사용 안함
-	swapChainDesc.AlphaMode = DXGI_ALPHA_MODE_IGNORE; // 투명도 조작 무시 | recommand for flip mode ?
-	swapChainDesc.Stereo = FALSE;
-	swapChainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH; // 전체 화면 전환을 허용
-	swapChainDesc.Scaling = DXGI_SCALING_NONE; // 창의 크기와 백 버퍼의 크기가 다를 때. 백버퍼 크기에 맞게 스케일링 하지 않는다.
-
-	HR_T(pFactory->CreateSwapChainForHwnd
-	(
-		m_pDevice,
-		m_hWnd,
-		&swapChainDesc,
-		nullptr,
-		nullptr,
-		&m_pSwapChain
-	));
-
-	// 3. 랜더타겟 뷰 생성. 랜더타겟 뷰는 "여기에 그림을 그려라"라고 GPU에게 알려주는 역할을 하는 객체
-	// 텍스쳐와 영구적으로 연결되는 객체
-	ComPtr<ID3D11Texture2D> pBackBufferTexture;
+	// 4. 렌더타겟뷰 생성.  (백버퍼를 이용하는 렌더타겟뷰)	
+	ID3D11Texture2D* pBackBufferTexture = nullptr;
 	HR_T(m_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&pBackBufferTexture));
-	HR_T(m_pDevice->CreateRenderTargetView(pBackBufferTexture.Get(), nullptr, &m_pRenderTargetView));
+	HR_T(m_pDevice->CreateRenderTargetView(pBackBufferTexture, NULL, &m_pRenderTargetView));  // 텍스처는 내부 참조 증가
+	SAFE_RELEASE(pBackBufferTexture);	//외부 참조 카운트를 감소시킨다.
+	// 렌더 타겟을 최종 출력 파이프라인에 바인딩합니다.
+	m_pDeviceContext->OMSetRenderTargets(1, &m_pRenderTargetView, NULL);
 
-#if !USE_FLIPMODE
-	m_pDeviceContext->OMSetRenderTargets(1, &m_pRenderTargetView, nullptr); // ?
-#endif
+	// 뷰포트 설정.	
+	D3D11_VIEWPORT viewport = {};
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+	viewport.Width = (float)m_ClientWidth;
+	viewport.Height = (float)m_ClientHeight;
+	viewport.MinDepth = 0.0f;
+	viewport.MaxDepth = 1.0f;
+	m_pDeviceContext->RSSetViewports(1, &viewport);
 
 	return true;
 }
@@ -156,4 +129,89 @@ void TutorialApp::UninitD3D()
 	SAFE_RELEASE(m_pDeviceContext);
 	SAFE_RELEASE(m_pSwapChain);
 	SAFE_RELEASE(m_pDevice);
+}
+
+bool TutorialApp::InitScene()
+{
+	HRESULT hr = S_OK;
+	ID3D10Blob* errorMessage = nullptr;	// 컴파일 에러 메세지가 저장될 버퍼.
+
+	// 1. Render()에서 파이프 라인에 바인딩할 버텍스 버퍼와 정보 준비
+	// 아직 VertexShader의 World, View, Projection 변환을 사용하지 않으므로
+	// 직접 Normalized Device Coordinate의 위치로 설정
+	//      /---------------------(1,1,1)   z값은 깊이값
+	//     /                      / |   
+	// (-1,1,0)----------------(1,1,0)        
+	//   |        v1           |   |
+	//   |       /   \         |   |       중앙이 (0,0,0)  
+	//   |      /  +   \       |   |
+	//   |     /         \     |   |
+	//	 |   v0-----------v2   |  /
+	// (-1,-1,0)-------------(1,-1,0)
+	Vertex vertices[] =
+	{
+		Vector3(-0.5,-0.5,0.5), // v0    
+		Vector3(0,0.5,0.5),		// v1    
+		Vector3(0.5,-0.5,0.5),	// v2		
+	};
+	
+	D3D11_BUFFER_DESC vbDesc = {};
+	m_VertexCount = ARRAYSIZE(vertices);				// 정점 개수
+	vbDesc.ByteWidth = sizeof(Vertex) * m_VertexCount;	// 버텍스 버퍼의 크기 (Byte).
+	vbDesc.CPUAccessFlags = 0;							// 0 == CPU 엑세스 필요하지 않음
+	vbDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;		// 정점 버퍼로 사용.
+	vbDesc.MiscFlags = 0;
+	vbDesc.Usage = D3D11_USAGE_DEFAULT;					// CPU 접근 불가, GPU에서 읽기/쓰기로 가능한 버퍼로 생성
+
+	// 정점 버퍼 생성
+	D3D11_SUBRESOURCE_DATA vbData = {};
+	vbData.pSysMem = vertices; // 버퍼를 생성할 때 복사할 데이터의 주소 설정
+	HR_T(hr = m_pDevice->CreateBuffer(&vbDesc, &vbData, &m_pVertexBuffer));
+	
+	// 버텍스 버퍼 정보
+	m_VertexBufferStride = sizeof(Vertex);	// 버텍스 하나의 크기 설정
+	m_VertexBufferOffset = 0;				// 버텍스 시작 주소에서 더 할 오프셋 주소 
+
+	// 2. Render에서 파이프라인에 바인딩할 버텍스 셰이더 생성
+	ID3DBlob* vertexShaderBuffer = nullptr;	// 버텍스 셰이더 HLSL의 컴파일된 결과(바이트 코드)를 담을 수 있는 버퍼 객체
+	HR_T(CompileShaderFromFile(L"BasicVertexShader.hlsl", "main", "vs_4_0", &vertexShaderBuffer));
+	HR_T(m_pDevice->CreateVertexShader(vertexShaderBuffer->GetBufferPointer(), // 필요한 데이터 복사하며 객체 생성
+									   vertexShaderBuffer->GetBufferSize(), 
+									   NULL, &m_pVertexShader));
+
+	// 3. Render()에서 파이프라인에 바인딩할 InputLayout 생성
+	// 
+	// 인풋 레이아웃은 버텍스 셰이더가 입력받을 데이터 형식을 지정한다.
+	D3D11_INPUT_ELEMENT_DESC layout[] =
+	{	// SemanticName , SemanticIndex , Format , InputSlot , AlignedByteOffset , InputSlotClass , InstanceDataStepRate	
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0}
+	};
+
+	// 버텍스 셰이더의 Input에 지정된 내용과 같은지 검증하면서 InputLayout 생성
+	HR_T(hr = m_pDevice->CreateInputLayout(layout, 
+										   ARRAYSIZE(layout),
+										   vertexShaderBuffer->GetBufferPointer(), 
+										   vertexShaderBuffer->GetBufferSize(), 
+										   &m_pInputLayout));
+
+	SAFE_RELEASE(vertexShaderBuffer); // 복사했으니 버퍼는 해제 가능
+
+	// 4. Render에서 파이프라인에 바인딩할 픽셀 셰이더 생성
+	ID3DBlob* pixelShaderBuffer = nullptr;	// 픽셀 셰이더 HLSL의 컴파일된 결과(바이트 코드)를 담을 수 있는 객체
+	HR_T(CompileShaderFromFile(L"BasicPixelShader.hlsl", "main", "ps_4_0", &pixelShaderBuffer));
+	HR_T(m_pDevice->CreatePixelShader(pixelShaderBuffer->GetBufferPointer(), 
+									  pixelShaderBuffer->GetBufferSize(), 
+									  NULL, &m_pPixelShader));
+
+	SAFE_RELEASE(pixelShaderBuffer); // 복사했으니 버퍼는 해제 가능
+
+	return true;
+}
+
+void TutorialApp::UninitScene()
+{
+	SAFE_RELEASE(m_pVertexBuffer);
+	SAFE_RELEASE(m_pInputLayout);
+	SAFE_RELEASE(m_pVertexShader);
+	SAFE_RELEASE(m_pPixelShader);
 }
--- D3D11_1/DrawTriangle/TutorialApp.h
+++ D3D11_1/DrawTriangle/TutorialApp.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 #include <d3d11.h>
 #include <dxgi1_2.h>
 
@@ -10,10 +10,20 @@ public:
 	TutorialApp(HINSTANCE hInstance);
 	~TutorialApp();
 
+	// 렌더링 파이프라인을 구성하는 필수 객체 인터페이스 ( 뎁스 스텐실 뷰는 사용 안함 )
 	ID3D11Device* m_pDevice = nullptr;						// 디바이스
 	ID3D11DeviceContext* m_pDeviceContext = nullptr;		// 디바이스 컨텍스트
-	IDXGISwapChain1* m_pSwapChain = nullptr;				// 스왑체인 
+	IDXGISwapChain* m_pSwapChain = nullptr;					// 스왑체인 
 	ID3D11RenderTargetView* m_pRenderTargetView = nullptr;	// 랜더 타겟
+
+	// 렌더링 파이프라인에 적용하는 객체 정보
+	ID3D11VertexShader* m_pVertexShader = nullptr;		// 정점 셰이더
+	ID3D11PixelShader* m_pPixelShader = nullptr;		// 픽셀 셰이더
+	ID3D11InputLayout* m_pInputLayout = nullptr;		// 입력 레이아웃
+	ID3D11Buffer* m_pVertexBuffer = nullptr;			// 버텍스 버퍼
+	UINT m_VertexBufferStride = 0;						// 버텍스 하나의 크기
+	UINT m_VertexBufferOffset = 0;						// 버텍스 버퍼의 오프셋
+	UINT m_VertexCount = 0;								// 버텍스 개수
 
 	virtual bool Initialize(UINT Width, UINT Height);	// 윈도우 정보는 게임 마다 다를수 있으므로 등록,생성,보이기만 한다.
 	virtual void Update();
@@ -21,4 +31,7 @@ public:
 
 	bool InitD3D();
 	void UninitD3D();
+
+	bool InitScene();
+	void UninitScene();
 };
