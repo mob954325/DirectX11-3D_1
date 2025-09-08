19bde3e7bdafed0ad2704e08d089d4a3e12395f8
mob954325 <lsh954325@gmail.com>
2025-09-08 11:22:39 +0900

    DrawMesh 프로젝트 추가

---
+++ D3D11_1/DrawMesh/DrawMeshApp.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <d3d11.h>
+#include <dxgi1_2.h>
+#include <wrl/client.h>
+
+#include "../Common/GameApp.h"
+
+using namespace Microsoft::WRL;
+
+class DrawMeshApp : public GameApp
+{
+public:
+	DrawMeshApp(HINSTANCE hInstance);
+	~DrawMeshApp();
+
+	ComPtr<ID3D11Device> m_pDevice = nullptr;						// 디바이스
+	ComPtr<ID3D11DeviceContext> m_pDeviceContext = nullptr;			// 디바이스 컨텍스트
+	ComPtr<IDXGISwapChain1> m_pSwapChain = nullptr;					// 스왑체인 
+	ComPtr<ID3D11RenderTargetView> m_pRenderTargetView = nullptr;	// 랜더 타겟
+
+	virtual bool Initialize(UINT Width, UINT Height);	// 윈도우 정보는 게임 마다 다를수 있으므로 등록,생성,보이기만 한다.
+	virtual void Update();
+	virtual void Render();
+
+	bool InitD3D();
+};
