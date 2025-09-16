91312a3944458a504c2b0fa815d89c76ba7dccf1
mob954325 <lsh954325@gmail.com>
2025-09-16 12:14:19 +0900

    chore - 기본 색상 제거 및 코드 정리

--- D3D11_1/DrawTextureAndSkybox/BasicPixelShader.hlsl
+++ D3D11_1/DrawTextureAndSkybox/BasicPixelShader.hlsl
@@ -7,9 +7,8 @@ float4 main(PS_INPUT input) : SV_TARGET
     
     // color
     float4 finalColor = 0;    
-    float4 BaseColor = (1.0f, 1.0f, 1.0f, 1.0f);
-    finalColor = BaseColor * saturate(dot((float3) LightDirection, input.Norm) * LightColor);
+    finalColor = finalTexture * saturate(dot((float3) LightDirection, input.Norm) * LightColor);
     finalColor.a = 1;    
     
-    return finalTexture * finalColor;
+    return finalColor;
 } 
