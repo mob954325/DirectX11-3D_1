c624080275987e43af3c4b443aeca8bf382b742c
Lee SeongHo <lsh954325@gmail.com>
2025-09-23 23:25:28 +0900

    feat - blinn phong 계산 값 추가
    ps hlsl에 일단 추가됨

--- D3D11_1/PhongShading/LightPixelShader.hlsl
+++ D3D11_1/PhongShading/LightPixelShader.hlsl
@@ -7,22 +7,25 @@ float4 main(PS_INPUT input) : SV_TARGET
     // Phong
     // I = k_a * l_a + k_d(N dot L) I_l + k_s (R dot V)^a * I_l   
     // 최종 출력 값 = 환경광 + 확산광 + 정반사광    
-    // 빛의 광들과 머터리얼의 광들을 dot의 각도에 따라서 색을 결정함
-    
-    float3 lightDir = -LightDirection;
+    // 빛의 광들과 머터리얼의 광들을 dot의 각도에 따라서 색을 결정함    
     
     float4 finalAmbient = matAmbient * LightAmbient;   
     float3 norm = normalize(input.Norm);
-    float diffuseFactor = dot(lightDir, norm);   
+    float diffuseFactor = dot(-LightDirection, norm);   
     
     float4 finalDiffuse = float4(0.0f, 0.0f, 0.0f, 0.0f);
     float4 finalSpecular = float4(0.0f, 0.0f, 0.0f, 0.0f);
     
     if (diffuseFactor > 0.0f)
-    {    
-        float3 v = normalize(reflect((float3) LightDirection, norm)); // reflect값이 정규화된 값이 아님 
-        float specFactor = pow(max(dot(v, normalize(CameraPos - input.World)), 0.0f), Shininess);
-    
+    {   
+        // Phong
+        // float3 v = reflect(normalize((float3)LightDirection), norm); // 
+        // float specFactor = pow(max(dot(v, normalize(CameraPos - input.World)), 0.0f), Shininess);
+
+        // Blinn-Phong
+        float3 halfVector = normalize(-LightDirection + (CameraPos - input.World));
+        float specFactor = pow(saturate(dot(norm, halfVector)), Shininess);
+
         finalDiffuse = diffuseFactor * matDiffuse * LightDiffuse;
         finalSpecular = specFactor * matSpecular * LightSpecular;
     }  
