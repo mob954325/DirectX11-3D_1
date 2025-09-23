9fa7663b818f24247dbd2bd9a2ad9a008d89c30c
mob954325 <lsh954325@gmail.com>
2025-09-23 18:43:27 +0900

    chore - 주석 추가

--- D3D11_1/PhongShading/LightPixelShader.hlsl
+++ D3D11_1/PhongShading/LightPixelShader.hlsl
@@ -7,6 +7,7 @@ float4 main(PS_INPUT input) : SV_TARGET
     // Phong
     // I = k_a * l_a + k_d(N dot L) I_l + k_s (R dot V)^a * I_l   
     // 최종 출력 값 = 환경광 + 확산광 + 정반사광    
+    // 빛의 광들과 머터리얼의 광들을 dot의 각도에 따라서 색을 결정함
     
     float3 lightDir = -LightDirection;
     
@@ -19,8 +20,8 @@ float4 main(PS_INPUT input) : SV_TARGET
     
     if (diffuseFactor > 0.0f)
     {    
-        float3 v = normalize(reflect((float3) LightDirection, norm));
-        float specFactor = pow(max(dot(v, normalize(CameraPos - input.World)), 0.0f), Shininess); // ??
+        float3 v = normalize(reflect((float3) LightDirection, norm)); // reflect값이 정규화된 값이 아님 
+        float specFactor = pow(max(dot(v, normalize(CameraPos - input.World)), 0.0f), Shininess);
     
         finalDiffuse = diffuseFactor * matDiffuse * LightDiffuse;
         finalSpecular = specFactor * matSpecular * LightSpecular;
