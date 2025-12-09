3aedf297855eeffaebaf70614cb4e248d99870ba
mob954325 <lsh954325@gmail.com>
2025-12-09 16:58:01 +0900

    fix - 잘못된 계산식 수정,
    - 그림자 코드 추가
    - FresnelSchlick -> cosTheta와 F0을 매개변수로 하도록 수정
    - IBL 단계에서 finalMetalness, finalRoughness 쓰도록 수정 ( 샘플링한 텍스처 기준이 아니라서 이상하게 나왔었음 )
    - 모든 오브젝트가 roughness와 metalness값에 영향받도록 수정 ( 0 -> 1 )

--- D3D11_1/18_PBR1/Shaders/PS_PBR.hlsl
+++ D3D11_1/18_PBR1/Shaders/PS_PBR.hlsl
@@ -16,9 +16,9 @@ float NDFGGXTR(float3 normal, float3 halfVec, float roughness)
 }
 
 // F(v, h) Fresnel equation : Fresnel-Schlick approximation 
-float3 FresnelSchlick(float3 halfVec, float3 viewVec, float3 F0) // F0 == relfection factor
+float3 FresnelSchlick(float3 F0, float cosTheta) // F0 == relfection factor
 {
-    return F0 + (1.0 - F0) * pow(1.0 - saturate(dot(halfVec, viewVec)), 5.0);
+    return F0 + (1.0 - F0) * pow(1.0 - cosTheta, 5.0);
 }
 
 // G(l,v,h) 
@@ -43,26 +43,26 @@ float4 main(PS_INPUT input) : SV_TARGET
 {
     // 그림자처리 부분 =====================================================================
     float finalShadow = 1.0f;
-    //// 광원 NDC 좌표계에서는 좌표는 계산해주지 않으므로 계산한다.
-    //float currentShadowDepth = input.PositionShadow.z / input.PositionShadow.w;
-    //
-    //// 광원 NDC 좌표계에서의 x(-1 ~ 1), y(-1 ~ 1)
-    //float2 uv = input.PositionShadow.xy / input.PositionShadow.w;
-    //
-    //// NDC좌표계에서 Texture 좌표계로 변환
-    //uv.y = -uv.y;
-    //uv = uv * 0.5 + 0.5;
-    //
-    //if (uv.x >= 0.0 && uv.x <= 1.0 && uv.y >= 0.0 && uv.y <= 1.0)
-    //{
-    //    float sampleShadowDepth = txShadow.Sample(samLinear, uv).r;
-    //    
-    //    // currentShadowDepth가 더 크면 뒤 쪽에 있으므로 직접광 차단
-    //    if (currentShadowDepth > sampleShadowDepth + 0.001)
-    //    {
-    //        finalShadow = 0.0f;
-    //    }
-    //}
+    // 광원 NDC 좌표계에서는 좌표는 계산해주지 않으므로 계산한다.
+    float currentShadowDepth = input.PositionShadow.z / input.PositionShadow.w;
+    
+    // 광원 NDC 좌표계에서의 x(-1 ~ 1), y(-1 ~ 1)
+    float2 uv = input.PositionShadow.xy / input.PositionShadow.w;
+    
+    // NDC좌표계에서 Texture 좌표계로 변환
+    uv.y = -uv.y;
+    uv = uv * 0.5 + 0.5;
+    
+    if (uv.x >= 0.0 && uv.x <= 1.0 && uv.y >= 0.0 && uv.y <= 1.0)
+    {
+        float sampleShadowDepth = txShadow.Sample(samLinear, uv).r;
+        
+        // currentShadowDepth가 더 크면 뒤 쪽에 있으므로 직접광 차단
+        if (currentShadowDepth > sampleShadowDepth + 0.001)
+        {
+            finalShadow = 0.0f;
+        }
+    }
     
     // 광원처리 부분 =====================================================================
     // base(diffuse) texture Sampling 
@@ -106,19 +106,19 @@ float4 main(PS_INPUT input) : SV_TARGET
     float metalnessSample = txMetalness.Sample(samLinear, input.Tex).r; // grayScale
     if(!hasMetalness)
     {
-        metalnessSample = 0;
+        metalnessSample = 1;
     }
     
-    float finalMetalness = metalnessSample + Metalness;
+    float finalMetalness = metalnessSample * Metalness;
     
     // RoughnessSample
     float roughnessSample = txRoughness.Sample(samLinear, input.Tex).r; // grayScale
     if(!hasRoughness)
     {
-        roughnessSample = hasShininess ? 1 - roughnessSample : 0;
+        roughnessSample = hasShininess ? 1 - roughnessSample : 1;
     }
     
-    float finalRoughness = roughnessSample + Roughness;
+    float finalRoughness = roughnessSample * Roughness;
 
     float3 directLighting = 0.0f;
     
@@ -137,7 +137,7 @@ float4 main(PS_INPUT input) : SV_TARGET
     
     {   
         float D = NDFGGXTR(norm, Lh, max(0.001, finalRoughness));
-        float F = FresnelSchlick(Lh, Lo, F0);
+        float F = FresnelSchlick(F0, max(0, dot(Lh, Lo)));
         float G = GSmithMethod(norm, Lo, Li, finalRoughness);
    
         // 표면 산란
@@ -159,24 +159,24 @@ float4 main(PS_INPUT input) : SV_TARGET
         float3 irradiance = txIBLIrradiance.Sample(samLinear, norm).rgb;
     
         // IBL diffuse    
-        float3 F = FresnelSchlick(norm, Lo, F0);
-        float3 kd = lerp(1.0 - F, 0.0, Metalness);
+        float3 F = FresnelSchlick(F0, NdotO);
+        float3 kd = lerp(1.0 - F, 0.0, finalMetalness);
         
-        float3 diffuseIBL = kd * (float3)albedo * PI * irradiance; //
+        float3 diffuseIBL = kd * (float3)albedo * irradiance / PI; // irradiance map이 1/pi가 포함되어있으면 제거 있으면 1/pi 추가하기
     
         // IBL specular        
         uint specularTexureLevels, width, height;        
         txIBLSepcualar.GetDimensions(0, width, height, specularTexureLevels);
         
-        float3 PrefilteredColor = txIBLSepcualar.SampleLevel(samLinear, reflect(-Lo, norm), Roughness * specularTexureLevels).rgb;
+        float3 PrefilteredColor = txIBLSepcualar.SampleLevel(samLinear, reflect(-Lo, norm), finalRoughness * specularTexureLevels).rgb;
         
         // dot(Normal,View) , roughness를 텍셀좌표로 미리계산된 F*G , G 평균값을 샘플링한다  
-        float2 specularBRDF = txIBLLookUpTable.Sample(samLinear, float2(NdotO, Roughness)).rg;
+        float2 specularBRDF = txIBLLookUpTable.Sample(samLinear, float2(NdotO, finalRoughness)).rg;
         
         // 쿡토런스 Spceular BRDF 근사식
-        float3 specularIBL = PrefilteredColor * (F0 * specularBRDF.x + specularBRDF.y); // ??
+        float3 specularIBL = PrefilteredColor * (F0 * specularBRDF.x + specularBRDF.y); // x : normal dot view, y : roughtness
         
-        inDirectLighting = diffuseIBL + specularIBL; // * AmbientOcclusion;
+        inDirectLighting = (diffuseIBL + specularIBL); // * AmbientOcclusion;
     }
     
     return float4(pow(float3(directLighting + inDirectLighting), 1.0 / 2.2), 1.0); // linear -> gamma 
